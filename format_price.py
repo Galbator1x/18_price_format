f2beac3b0e2684c2fdf58798b9444be8208b1df0
Ivan Filimonov <snake73w@gmail.com>
2016-10-13 05:25:14 +0700

    Added script.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,30 @@
+import re
+import locale
+
+
 def format_price(price):
-    pass
+    price = str(price).replace(',', '.')
+    price = re.sub(r'\s', '', price)
+    price = float(price)
+
+    loc = locale.getlocale()
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+    price = locale.currency(price, grouping=True)
+    price = re.sub(r'\.00$', '', price)
+    price = price.replace('$', '')
+    price = price.replace(',', ' ')
+
+    locale.setlocale(locale.LC_ALL, loc)
+    return price
+
 
 if __name__ == '__main__':
-    pass
+    while True:
+        price = input('Enter the price: ')
+        if not price:
+            break
+        try:
+            print(format_price(price))
+        except ValueError:
+            print('Incorrect number.')
